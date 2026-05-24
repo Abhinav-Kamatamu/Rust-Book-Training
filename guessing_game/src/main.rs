3a3da8ac71658c6ccd6b33c7875596f85a3235e8
Stranger <abhinav.kamatamu@gmail.com>
2026-05-23 23:59:30 -0400

    Guessing game completed.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,16 +1,36 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    printl!("The secret number is: {secret number}");
+    println!("The secret number is: {secret_number}");
 
-    let mut guess = String""new();
+    let mut guess = String::new();
 
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
-    
+
+    let guess : u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+
+    };
+
+    loop {
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+
     println!("You guessed: {guess}");
 }
